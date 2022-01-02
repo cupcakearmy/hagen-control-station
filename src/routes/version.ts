ec5b7eafaa369572017634d4d29613503a8a49ed
cupcakearmy <hi@nicco.io>
2022-01-02 14:42:46 +0100

    version bump

--- src/routes/version.ts
+++ src/routes/version.ts
@@ -4,7 +4,6 @@ export const Version = process.env['npm_package_version']
 
 export function init(bot: Bot) {
   bot.command('/version', async (ctx) => {
-    // ctx.reply('Version: ' + version)
     ctx.reply(`Version: ${Version}`)
   })
 }
